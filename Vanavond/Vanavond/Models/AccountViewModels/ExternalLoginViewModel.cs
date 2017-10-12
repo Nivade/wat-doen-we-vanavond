6a41bb3f7913c8f13a10a5cb3a4d5b14c284b0b6
Nivade <nhavandeursen@gmail.com>
2017-10-12 15:03:05 +0200

    Add givenname, surname and nickname fields.

--- Vanavond/Vanavond/Models/AccountViewModels/ExternalLoginViewModel.cs
+++ Vanavond/Vanavond/Models/AccountViewModels/ExternalLoginViewModel.cs
@@ -11,5 +11,11 @@ namespace Vanavond.Models.AccountViewModels
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+
+        [RegularExpression(@"[A-z\s]*")]
+        public string Nickname { get; set; }
     }
 }
